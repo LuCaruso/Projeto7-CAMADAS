2258370f5a9822959c4d3c57ccf51fc9c13b828e
LuCaruso <lucac@al.insper.edu.br>
2023-10-27 16:02:41 -0300

    Encode Final

--- encode_versaoAlunos.py
+++ encode_versaoAlunos.py
@@ -15,37 +15,58 @@ def todB(s):
     sdB = 10*np.log10(s)
     return(sdB)
 
-
-
-
 def main():
-    
-   
     #********************************************instruções*********************************************** 
     # seu objetivo aqui é gerar duas senoides. Cada uma com frequencia corresposndente à tecla pressionada
     # então inicialmente peça ao usuário para digitar uma tecla do teclado numérico DTMF
+    num = int(input("Digite um numero de 0 a 9: "))
+    NUM = num
     # agora, voce tem que gerar, por alguns segundos, suficiente para a outra aplicação gravar o audio, duas senoides com as frequencias corresposndentes à tecla pressionada, segundo a tabela DTMF
+
     # Essas senoides tem que ter taxa de amostragem de 44100 amostras por segundo, entao voce tera que gerar uma lista de tempo correspondente a isso e entao gerar as senoides
     # Lembre-se que a senoide pode ser construída com A*sin(2*pi*f*t)
     # O tamanho da lista tempo estará associada à duração do som. A intensidade é controlada pela constante A (amplitude da senoide). Construa com amplitude 1.
+    fs = 44100
+    freq = [[941, 1336], [697, 1209], [697, 1336], [697, 1477], [770, 1209], [770, 1336], [770, 1477], [852, 1209], [852, 1336], [852, 1477]]
+    t = np.linspace(0, 5, 44100*5)
+
+    sen1 = 1*np.sin(2*np.pi*freq[num][0]*t)
+    sen2 = 1*np.sin(2*np.pi*freq[num][1]*t)
+
     # Some as senoides. A soma será o sinal a ser emitido.
+    tone = sen1 + sen2
+
     # Utilize a funcao da biblioteca sounddevice para reproduzir o som. Entenda seus argumento.
     # Grave o som com seu celular ou qualquer outro microfone. Cuidado, algumas placas de som não gravam sons gerados por elas mesmas. (Isso evita microfonia).
-    
     # construa o gráfico do sinal emitido e o gráfico da transformada de Fourier. Cuidado. Como as frequencias sao relativamente altas, voce deve plotar apenas alguns pontos (alguns periodos) para conseguirmos ver o sinal
     
-
     print("Inicializando encoder")
     print("Aguardando usuário")
     print("Gerando Tons base")
     print("Executando as senoides (emitindo o som)")
     print("Gerando Tom referente ao símbolo : {}".format(NUM))
     sd.play(tone, fs)
+    sd.wait()
     # Exibe gráficos
-    plt.show()
     # aguarda fim do audio
     sd.wait()
-    plotFFT(self, signal, fs)
+
+    # Plota o gráfico no domínio da frequência da soma das frequências somadas
+    sinal = signalMeu()
+    sinal.plotFFT(tone, fs)
+    plt.xlabel('Frequência [Hz]')
+    plt.ylabel('Amplitude')
+    plt.xlim(500, 2000)
+
+
+    # Plota o gráfico no domínio do tempo da soma das frequências somadas
+    plt.figure()
+    plt.plot(t[:1000], tone[:1000])
+    plt.title('Domínio do Tempo da Soma')
+    plt.xlabel('Tempo [s]')
+    plt.ylabel('Amplitude')
+    plt.grid(True)
+    plt.show()
     
 
 if __name__ == "__main__":
